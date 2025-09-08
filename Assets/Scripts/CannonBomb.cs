4e6427d14a8f39cd6acabc3ca240de11476b70c2
JeronimoLedesma <ledesmajero@gmail.com>
2025-09-08 19:40:33 -0300

    Time_Update

--- Assets/Scripts/CannonBomb.cs
+++ Assets/Scripts/CannonBomb.cs
@@ -31,7 +31,7 @@ public class CannonBomb : MonoBehaviour
 
     IEnumerator report_and_Die()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(3);
         SimulationManager.createReport();
         Destroy(gameObject);
     }
